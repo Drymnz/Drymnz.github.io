f67aabaf8cb729ff3d1a875fa390408d85cea0c3
Benjamìn de Jesùs <mcdreck276@gmail.com>
2025-01-05 10:36:47 -0600

    Mejoras para las tecnologias en diseño css

--- src/components/ProgressCard.jsx
+++ src/components/ProgressCard.jsx
@@ -1,28 +1,27 @@
 import "../components/ProgressCard.css";
 
-// eslint-disable-next-line react/prop-types
 function ProgressCard({ title, img, percentage }) {
   return (
     <div className="contentContainer">
       <div className="skillBar">
-        <h4>{title}</h4>
         <img src={img} alt={title} />
-        <div className="bar">
-          <div className="skillBarContainer">
-            <div
-              className="skillBarValue "
-              style={{
-                width: `${percentage}%`,
-              }}
-            ></div>
-          </div>
-        </div>
+        <h4>{title}</h4>
       </div>
     </div>
   );
 }
 
+
 //Desarrollador Junior: Porcentaje aproximado: alrededor del 20% al 40%.
 //Desarrollador Senior: Porcentaje aproximado: alrededor del 70% al 90% o más.
 
 export default ProgressCard;
+
+//<div className="skillBarContainer">
+//          <div
+//            className="bar"
+//            style={{
+//              width: `${percentage}%`,
+//            }}
+//          ></div>
+//        </div>
